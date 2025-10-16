391a77012298a59ee40775adaefa9daed74ee0ca
CarineEpitech <carine.agboton@epitech.eu>
2025-10-16 17:56:21 +0100

    fix : fix branch name issues

--- useful_api/routes/api.php
+++ useful_api/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 
-Route::middleware('auth:sanctum')->group(function (){
+Route::middleware('auth:sanctum')->group(function (){ 
     Route::get('/modules', [ModuleController::class,'show']);
     Route::post('/modules/{id}/activate', [ModuleController::class, "isActivated"]);
     Route::post('/modules/{id}/deactivate', [ModuleController::class,"isDeactivated"]);
